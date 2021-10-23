cba0fe0078e04a5a5f25fd2081773d35e9d11d60
anacleto-caua <anacleto.for.work@gmail.com>
2021-10-23 09:27:19 -0300

    first commit

---
+++ database/migrations/2021_10_23_112835_create_links_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('links', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 25);
+            $table->string('link');
+            $table->foreignId('trees');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('links');
+    }
+}
---
+++ database/migrations/2021_10_23_112853_create_trees_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTreesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trees', function (Blueprint $table) {
+            $table->id();
+            $table->string('address', 25); #address vs name
+            $table->string('title', 100);
+            $table->string('description');
+            $table->foreignId('creator');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trees');
+    }
+}
