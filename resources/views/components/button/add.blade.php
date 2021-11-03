7194e7f943bd5984c6b075516e83986a4794c128
anacleto-caua <anacleto.for.work@gmail.com>
2021-11-03 11:25:51 -0300

    components personalization

---
+++ resources/views/components/button/add.blade.php
@@ -0,0 +1,3 @@
+<button {{ $attributes->merge(['type' => 'button', 'class' => 'items-center px-4 py-3 my-3 bg-green-800 border border-transparent rounded-md font-semibold text-xs text-white tracking-widest disabled:opacity-25 transition ease-in-out duration-150']) }}>
+    {{ $slot }}
+</button>
--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,3 +1,18 @@
-@props(['disabled' => false])
+@props(['title', 'atrib', 'type'])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50']) !!}>
+<div {{ $attributes }}>
+    <label for="{{ $atrib }}" class="block font-medium text-sm text-gray-700">
+        {{ $title ?? Str::ucfirst($atrib) }}
+    </label>
+    
+    <input
+        id="{{ $atrib }}"
+        name="{{ $atrib }}"
+        type="{{ $type ?? 'text' }}"
+        :value="{{ old($atrib) }}"
+        {!! $attributes->merge(
+            ['class' => 'rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 w-full mt-2 mb-3 mx-auto']
+        ) !!}
+         autofocus
+         />
+</div>
